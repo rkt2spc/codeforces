dbc8ca706a6424bd757a18276d1219bef18db409
Tuan Nguyen <nmtuan.dev@gmail.com>
2021-10-06 05:42:46 +0800

    263A

---
+++ 263A/Solution.java
@@ -0,0 +1,78 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public void solve(InputReader in, PrintWriter out) {
+        for (int i = 1; i <= 5; ++i) {
+            for (int j = 1; j <= 5; ++j) {
+                if (in.nextInt() == 1) {
+                    out.println(Math.abs(3 - i) + Math.abs(3 - j));
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        Solution sln = new Solution();
+        sln.solve(in, out);
+
+        out.close();
+    }
+
+    private static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        public InputReader() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public InputReader(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        public boolean hasMoreTokens() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                String s = nextLine();
+                if (s == null)
+                    return false;
+                tokenizer = new StringTokenizer(s);
+            }
+            return true;
+        }
+
+        public String nextToken() {
+            return hasMoreTokens() ? tokenizer.nextToken() : null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextToken());
+        }
+
+        public String nextLine() {
+            try {
+                tokenizer = null;
+                return reader.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
